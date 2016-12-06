3bbf00fc195c405e0830aa70b982df173d7b136b
Michael Thiessen <michael.thiessen@esentire.com>
2016-12-06 08:21:20 -0500

    Factored color utilities out

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ class App extends Component {
     return (
       <div className="App">
         <div>
+          $base-color:&nbsp;
         </div>
         <div>
+          $darker:&nbsp;
         </div>
         <div>
+          $darkest:&nbsp;
         </div>
         <div>
+          $nearblack:&nbsp;
         </div>
         <div>
+          $lighter:&nbsp;
         </div>
         <div>
+          $lightest:&nbsp;
         </div>
       </div>
     );
