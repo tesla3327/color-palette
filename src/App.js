061ea384b0835ba15efb0ba42b416faf07e76c2f
Michael Thiessen <michael.thiessen@esentire.com>
2016-12-06 08:55:48 -0500

    Added screenshots

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import React, { Component } from 'react';
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div>
-          $base-color:&nbsp;
-        </div>
-        <div>
-          $darker:&nbsp;
-        </div>
-        <div>
-          $darkest:&nbsp;
-        </div>
-        <div>
-          $nearblack:&nbsp;
-        </div>
-        <div>
-          $lighter:&nbsp;
-        </div>
-        <div>
-          $lightest:&nbsp;
-        </div>
-      </div>
-    );
-  }
-}
+const vars = [
+  '$base-color',
+  '$darker',
+  '$darkest',
+  '$nearblack',
+  '$lighter',
+  '$lightest',
+];
+
+const App = () => (
+  <div className="App">
+    {
+      vars.map( e => (<div>{ `${e}: ` }</div>))
+    }
+  </div>
+);
 
 export default App;
